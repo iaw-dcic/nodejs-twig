714d3fc747f4de45d990eee86f8e71adb1fcc0c5
Mariano Tucat <mariano@skedgo.com>
2018-05-01 23:21:38 -0300

    authMiddleware

--- app_server/routes/api.js
+++ app_server/routes/api.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const pizzaApi = require('../controllers/pizzaApi');
 const pedidoApi = require('../controllers/pedidoApi');
+const middleware = require('../auth/middleware');
 
 /* GET home page. */
 router.get('/pizzas', pizzaApi.getPizzas);
-router.get('/pedidos', pedidoApi.getPedido);
-router.post('/pedidos', pedidoApi.setPedido);
+router.get('/pedidos', middleware, pedidoApi.getPedido);
+router.post('/pedidos', middleware, pedidoApi.setPedido);
 
 module.exports = router;
